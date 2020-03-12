1f1690e875473b9064855bb8760dc10120097b04
zhoukang <zhoukang@xin.com>
2020-03-12 14:42:37 +0800

    羞愧，为啥debug 断点不管用了呢

--- objc4-779.1/studyobjc4/main.m
+++ objc4-779.1/studyobjc4/main.m
@@ -6,29 +6,30 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "MyExpriceObj.h"
-#import "TestObj.h"
-#import "NSObject+DLIntrospection.h"
+//#import "MyExpriceObj.h"
+//#import "TestObj.h"
+//#import "NSObject+DLIntrospection.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
 //        [MyExpriceObj saocaozuo];
-        
+        NSObject *obj = [[NSObject alloc] init];
+        NSLog(@"为啥打不上断点呢 %@", obj);
 //        NSLog(@"classes obj %@", [TestObj classes]);
-        NSLog(@"properties obj %@", [TestObj properties]);
-        NSLog(@"instanceVariables obj %@", [TestObj instanceVariables]);
-        NSLog(@"classMethods obj %@", [TestObj classMethods]);
-        NSLog(@"instanceMethods obj %@", [TestObj instanceMethods]);
-        NSLog(@"parentClassHierarchy obj %@", [TestObj parentClassHierarchy]);
+//        NSLog(@"properties obj %@", [TestObj properties]);
+//        NSLog(@"instanceVariables obj %@", [TestObj instanceVariables]);
+//        NSLog(@"classMethods obj %@", [TestObj classMethods]);
+//        NSLog(@"instanceMethods obj %@", [TestObj instanceMethods]);
+//        NSLog(@"parentClassHierarchy obj %@", [TestObj parentClassHierarchy]);
         
         
 //        NSLog(@"classes obj1 %@", [TestObj1 classes]);
-        NSLog(@"properties obj1 %@", [TestObj1 properties]);
-        NSLog(@"instanceVariables obj1 %@", [TestObj1 instanceVariables]);
-        NSLog(@"classMethods obj1 %@", [TestObj1 classMethods]);
-        NSLog(@"instanceMethods obj1 %@", [TestObj1 instanceMethods]);
-        NSLog(@"parentClassHierarchy obj1 %@", [TestObj1 parentClassHierarchy]);
+//        NSLog(@"properties obj1 %@", [TestObj1 properties]);
+//        NSLog(@"instanceVariables obj1 %@", [TestObj1 instanceVariables]);
+//        NSLog(@"classMethods obj1 %@", [TestObj1 classMethods]);
+//        NSLog(@"instanceMethods obj1 %@", [TestObj1 instanceMethods]);
+//        NSLog(@"parentClassHierarchy obj1 %@", [TestObj1 parentClassHierarchy]);
         
         [[NSRunLoop currentRunLoop] run];
     }
